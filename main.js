ba88ce7836474666ddc81cfc3860296fafb17c95
Charles <charles.eubanks@gmail.com>
2021-10-09 10:32:00 -0700

    fixed big bug & bumped version

--- main.js
+++ main.js
@@ -11,14 +11,14 @@ console.log(process.argv);
 let path = args[0];
 console.log(`target: ${path}`);
 let possibles = {};
-for (c in config.crates){
-    let crate = config.crates[c];
-    possibles[crate.name] = crate;
+for (c in config.crates) {
+        let crate = config.crates[c];
+        possibles[crate.name] = crate;
 }
 
 let new_path = (level, root, loc) => {
         let parts = [];
-        for (var i = 0;i < level;i++){
+        for (var i = 0; i < level; i++) {
                 parts.push('..');
         }
         parts.push(root);
@@ -26,65 +26,79 @@ let new_path = (level, root, loc) => {
         return parts.join("/");
 }
 
-let edit_toml = (toml_obj, level) =>  {
+let edit_toml = (toml_obj, level) => {
+        // delete version and set path where necessary
         for (crate in toml_obj.dependencies) {
                 let hit = possibles[crate];
-                if (hit){
-                       // console.log(`${crate} loc=${hit.loc} level=${level}`);
+                if (hit) {
                         let root = config.projects[hit.proj];
-                        let path = new_path(level,root,hit.loc);
-                        toml_obj.dependencies[crate] = { "path" : path};
+                        let path = new_path(level, root, hit.loc);
+                        if (toml_obj["dependencies"][crate]["version"]) {
+                                // explicit object
+                                delete (toml_obj["dependencies"][crate]["version"]);
+                                toml_obj["dependencies"][crate]["path"] = path;
+                        } else {
+                                // just a version string
+                                toml_obj["dependencies"][crate] = { "path": path }
+                        }
                 }
 
         }
         for (crate in toml_obj["dev-dependencies"]) {
                 let hit = possibles[crate];
-                if (hit){
-                       // console.log(`${crate} loc=${hit.loc} level=${level}`);
+                if (hit) {
+                        // console.log(`${crate} loc=${hit.loc} level=${level}`);
                         let root = config.projects[hit.proj];
-                        let path = new_path(level,root,hit.loc);
-                        toml_obj["dev-dependencies"][crate] = { "path" : path};
+                        let path = new_path(level, root, hit.loc);
+                        if (toml_obj["dependencies"][crate]["version"]) {
+                                // explicit object
+                                delete (toml_obj["dev-dependencies"][crate]["version"]);
+                                toml_obj["dev-dependencies"][crate]["path"] = path;
+                        } else {
+                                // just a version string
+                                toml_obj["dev-dependencies"][crate] = { "path": path }
+                        }
                 }
-
         }
         return otoml.stringify(toml_obj)
 }
+
 let recurse_dir = (ppath, level) => {
         fs.readdir(ppath, function (err, items) {
                 for (var i = 0; i < items.length; i++) {
                         var file = ppath + '/' + items[i];
                         //console.log("Start: " + file);
-        
+
                         try {
                                 let stats = fs.statSync(file);
                                 let isFile = stats.isFile();
                                 let isCargo = items[i] == "Cargo.toml";
                                 if (isFile & isCargo) {
                                         try {
-                                                let tomlStr = fs.readFileSync(file,"utf-8");
+                                                let tomlStr = fs.readFileSync(file, "utf-8");
                                                 console.log(file);
-        
+
                                                 let cargo = toml.parse(tomlStr);
-                                                let new_toml = edit_toml(cargo,level);
-                                                fs.writeFileSync(file,new_toml,"utf-8")
+                                                let new_toml = edit_toml(cargo, level);
+                                                fs.writeFileSync(file, new_toml, "utf-8")
                                         }
                                         catch (e2) {
                                                 console.log(e2);
                                         }
-        
+
                                 }
-                                else if (stats.isDirectory()){
-                                        recurse_dir(file,level+1);
+                                else if (stats.isDirectory()) {
+                                        recurse_dir(file, level + 1);
                                 }
-        
+
                         } catch (err) {
                                 console.log(err);
                         }
                 }
         });
-        
+
 };
-recurse_dir(path,0);
+recurse_dir(path, 0);
 
 
 
